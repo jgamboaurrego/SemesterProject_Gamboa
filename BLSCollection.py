cf07000a047d0aab7974d2e7f41d9caa2efd8b17
jgamboaurrego <jagamboa0358@gmail.com>
2024-12-17 13:11:08 -0600

    Revert "Hiding API Key"
    This reverts commit b894dab216670e0dee7729540a71f71a07dd08d6.

--- BLSCollection.py
+++ BLSCollection.py
@@ -2,43 +2,36 @@ import pandas as pd
 import requests
 import json
 from datetime import date
-from dotenv import load_dotenv
-import os
-load_dotenv()
 
 today = date.today()
 year = today.year
 
-key = os.getenv('API_KEY')
+key = "b30c43cc00de4e1ebf5a9674bdddefdb"
 url = "https://api.bls.gov/publicAPI/v2/timeseries/data/"
 def bls_data_r (series, latest,**kwargs):
-    # series is functional argument to allow a list of seriesID codes to be pulled from BLS
-    if not isinstance(series, list):
-        raise TypeError('series is not a list')
-    #**kwargs allows for multiple parameters to be added to the function such as startyear and endyear, this done such **kwargs allows for multiple key arguments like startyear: 2022 if function paramtert reads startyear = 2022
+    #as instructed by BLS keywords are written in this particular manner to call series id and regeistration key use the following key words to call additional information from BLS.
     if latest not in ('true', 'false'):
-        raise TypeError('latest is required true or false')
+        raise 'latest is required true or false'
     headers = {'Content-type': 'application/json'}
-    #as instructed by BLS keywords are written in this particular manner to call series id and regeistration key use the following keywords to call additional information from BLS.
-    p = {'seriesid': series, 'registrationKey': key, 'latest': latest,}
+    p = {'seriesid': series, 'registrationKey': key, 'latest': latest}
+    #kwargs allows the method to accept multiple arugments such as startyear, endyear, 'annualaverage'
     p.update(kwargs)
-    # allows for the additional keyword arguments like startyear and endyear to be included in the API request to BLS
     p = json.dumps(p)
     bls = requests.post(url, data= p, headers=headers)
     data = json.loads(bls.text)
     return data
 
+#sellection of various BLS series IDs  pulled from BLS
 series = ["CES0000000001", "LNS14000000","LNS11000000","LNS11300000", "SUUR0000SA0E"]
-#series of data from BLS being collected
 
-#following code checks if the "SemesterProject_Gamboa repository has bls_data
 username = "jgamboaurrego"
 repository = "SemesterProject_Gamboa"
 file_path = "bls_data.csv"
+
 urlg = f'https://api.github.com/repos/{username}/{repository}/contents/{file_path}'
+
 response = requests.get(urlg)
 
-#if response does have a csv with bls data then it will add new data to prevent accidental duplication if github workflow is run more than once a month it removes duplicate in csv file. If there is no bls data found it will pull history starting from 2022 to now
 if response.status_code == 200:
     json_data_l = bls_data_r(series,latest='true')
     bls_df_l = []
@@ -52,10 +45,10 @@ if response.status_code == 200:
     bls_df_l = pd.DataFrame(bls_df_l, columns = ['seriesId', 'year', 'period', 'value'])
     bls_df_l.to_csv("bls_data.csv", mode='a', header=False, index=False)
     bls_df_c = pd.read_csv("bls_data.csv")
-    bls_df_c.drop_duplicates(inplace=True) #removes duplicates if new BLS data isn't available or accidential refreash
-    bls_df_c.to_csv("bls_data.csv", index = False) # replaces bls_data.csv with clean results
+    bls_df_c.drop_duplicates(inplace=True)
+    bls_df_c.to_csv("bls_data.csv", index = False)
 else:
-    json_data =bls_data_r(series, latest= "false" ,startyear = 2022 , endyear = (year)) # pulls data from most resent year and starting year of 2022
+    json_data =bls_data_r(series, latest= "false" ,startyear = 2022 , endyear = (year))
     bls_df = []
     for series in json_data['Results']['series']:
         seriesId = series['seriesID']
